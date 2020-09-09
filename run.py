04f124d4782d9f2dcd65d8f68d8276facd0cda6f
Dermot-K <dermokeelan@gmail.com>
2020-09-09 13:32:56 +0000

    add index.html template

--- run.py
+++ run.py
@@ -1,30 +1,39 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)  # initialise new flask app
 messages = []   # empty list to which username:messages will be appended
 
 
 def add_messages(username, message):
-    messages.append("{}: {}".format(username, message))
+    """add messages to the 'messages' list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
+
+def get_all_messages():
+    """get all of the messages and separate them with a 'br'"""
+    return "<br>".join(messages)
 
 
 @app.route('/')  # app root decorator for index page
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "Welcome, {0}".format(username, messages)
+    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
     """Create a new message and redirect to the chat page"""
-    return "{0}: {1}".format(username, message)
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
